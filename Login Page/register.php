de832490cbd0d4aae456110b1329ebaf1fd9b240
lagkiller <chaub@bxscience.edu>
2016-11-29 19:34:32 -0500

    Add files via upload

--- Login Page/register.php
+++ Login Page/register.php
@@ -14,19 +14,9 @@
   
   if (isset($_POST['register']) && $_POST['register'] == 'Register') {
     
-    // @TODO: Check to see if duplicate usernames exist
+    // @TODO: Check to see if duplicate usernames exist FINISHED
     
-	if (isset($_POST['username']) ){
-		
-		$login_stmt = $dbconn->prepare('SELECT username FROM users_secure WHERE username=:username ');
-		$login_stmt->execute(array(':username' => $_POST['username']));
-  
-  
-		if ($_POST['username'] = $login_stmt->fetch()) {
-		$msg = "Username Taken";
-		}
-		
-	}
+	
     if (!isset($_POST['username']) || !isset($_POST['pass']) || !isset($_POST['passconfirm']) || empty($_POST['username']) || empty($_POST['pass']) || empty($_POST['passconfirm'])) {
       $msg = "Please fill in all form fields.";
     }
@@ -34,16 +24,30 @@
       $msg = "Passwords must match.";
     }
     else {
-      // Generate random salt
-      $salt = hash('sha256', uniqid(mt_rand(), true));      
+		if (isset($_POST['username']) ){
+			
+			$login_stmt = $dbconn->prepare('SELECT username FROM users_secure WHERE username=:username ');
+			$login_stmt->execute(array(':username' => $_POST['username']));
+	  
+	  
+			if ($_POST['username'] = $login_stmt->fetch()) {
+			$msg = "Username Taken";
+			}
+			else {
+				// Generate random salt
+				  $salt = hash('sha256', uniqid(mt_rand(), true));      
 
-      // Apply salt before hashing
-      $salted = hash('sha256', $salt . $_POST['pass']);
-      
-      // Store the salt with the password, so we can apply it again and check the result
-      $stmt = $dbconn->prepare("INSERT INTO users_secure (username, pass, salt) VALUES (:username, :pass, :salt)");
-      $stmt->execute(array(':username' => $_POST['username'], ':pass' => $salted, ':salt' => $salt));
-      $msg = "Account created.";
+				  // Apply salt before hashing
+				  $salted = hash('sha256', $salt . $_POST['pass']);
+				  
+				  // Store the salt with the password, so we can apply it again and check the result
+				  $stmt = $dbconn->prepare("INSERT INTO users_secure (username, pass, salt) VALUES (:username, :pass, :salt)");
+				  $stmt->execute(array(':username' => $_POST['username'], ':pass' => $salted, ':salt' => $salt));
+				  $msg = "Account created.";
+			}
+			
+		}
+
     }
   } 
 ?>
