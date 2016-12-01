77bdfd52ed4fd8e0f23ee06431984dd99f852215
dawneraq <dawneraq@gmail.com>
2016-12-01 18:14:10 -0500

    Style admin landing

--- adminlandingpage.php
+++ adminlandingpage.php
@@ -34,6 +34,8 @@ catch (Exception $e) {
 		<!-- Bootstrap.min.js -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<script src="resources/main.js"></script>
+		<!-- Bootstrap-select.css -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.min.css">
 		<link rel="stylesheet" href="resources/main.css">
 	</head>
 	<body>
@@ -58,7 +60,7 @@ catch (Exception $e) {
 			</div>
 		</nav>
 
-		<div class="container theme-showcase" role="main">
+		<div class="container theme-showcase main-content" role="main">
 			<div class="jumbotron">
 				<h1>FixxSquared</h1>
 				<p>Help campus residents! Or else!</p>
@@ -82,18 +84,25 @@ catch (Exception $e) {
 				</div>
 -->
 			</div>
-			<div class="container">
+			<div class="container row">
 				<div class="row">
 					<?php
 						foreach (getTickets() as $ticket) {
 							echo
 								'<div class="col-md-3">'.
 									'<div class="ticket">'.
-										'<h4>#'.$ticket['ticket_id'].': '.$ticket['summary'].'</h4>'.
+										'<h4><span class="ticket-id">'.$ticket['ticket_id'].'</span>: '.$ticket['summary'].'</h4>'.
 										'<h5>'.$ticket['residence_hall'].' '.$ticket['room'].'</h5>'.
 										'<strong>Request date:</strong> '.$ticket['request_date'].'<br/>'.
 										'<strong>Estimated completion time: </strong>'.$ticket['completion_time_estimated'].'<br/>'.
-										'<i>'.$ticketStatus[$ticket['status']].'</i>'.
+										'<div class="form-group"><select class="selectpicker form-control" title="'.$ticketStatus[$ticket['status']].'">';
+											foreach($ticketStatus as $status) {
+												echo '<option>'.
+														$status.
+													'</option>';
+											}
+							echo
+										'</select></div>'.
 										'<form method="post" action="adminlandingpage.php">
 										<button class= "btn btn-danger" name="delete" type="submit" value=" '.$ticket['ticket_id']  .'">Delete</button>
 										</form>'.
@@ -104,5 +113,29 @@ catch (Exception $e) {
 				</div>
 			</div>
 		</div>
+		
+		<!-- Bootstrap-select.js -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.min.js"></script>
+	
+	<script>
+		$(document).ready(function () {
+			$('.ticket').each(function () {
+				var ticketId = $(this).find('.ticket-id').text();
+				
+				$(this).find('.selectpicker')
+				.change(function () {
+					var newStatus = $(this).find('option:selected').text();
+					
+					//console.log('ticket ' + ticketId + ' is now ' + newStatus);
+					
+					var postData = { 'ticketId': ticketId, 'newStatus': newStatus };
+					
+					$.post('php/changeTicketStatus.php', postData, function (data) {
+						console.log(data);
+					});
+				});
+			});
+		});
+	</script>
 	</body>
 </html>
