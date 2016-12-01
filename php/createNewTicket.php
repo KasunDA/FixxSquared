d4344804ec78b73ee4c545cbfce6ec6800117b14
dawneraq <dawneraq@gmail.com>
2016-12-01 11:38:13 -0500

    Integrate createNewTicket with db

--- php/createNewTicket.php
+++ php/createNewTicket.php
@@ -13,7 +13,38 @@
 	}
 
 	function createNewTicket($config) {
-		// TODO
-		return "New ticket alert!";
+		try {
+			$connection = new PDO('mysql:host=localhost:3306', $config['DB_USERNAME'], $config['DB_PASSWORD']);
+			$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			if ($connection) {
+				$result = "";
+			}
+			else {
+				$error = "Unable to connect to database.";
+			}
+			
+			$statementCreateNewTicket = $connection->prepare(
+				"INSERT INTO fixx_squared.tickets (user_requesting_id, summary) VALUES (:userId, :ticketSummary);"
+			);
+			$statementCreateNewTicket->bindParam(':userId', $_SESSION['uid']);
+			$statementCreateNewTicket->bindParam(':ticketSummary', $_POST['summary']);
+			
+			if ($statementCreateNewTicket->execute()) {
+				$result .= "Ticket created successfully.\n";
+			}
+		}
+		catch (PDOException $e) {
+			$error = "PDO ERROR: ".$e->getMessage();
+		}
+		
+		if (isset($error)) {
+			return $error;
+		}
+		else if (isset($result)) {
+			return $result;
+		}
+		
+		return "ERROR: createNewTicket failed";
 	}
 ?>
